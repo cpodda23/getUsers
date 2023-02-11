c13d69deb0a370a33e0838e6107e3f4ca97c4fd0
cpodda23 <120722457+cpodda23@users.noreply.github.com>
2023-02-11 17:58:48 +0100

    1 file modified

--- index.js
+++ index.js
@@ -5,11 +5,24 @@ const getUsers = async () => {
 
 async function main() {
   const users = await getUsers();
-  const Samantha = users.find((user) => user.username === 'Samantha');
-  console.log(Samantha);
+  const newUsers = [
+    ...users.filter((user) => user.username !== 'Samantha'),
+    users.find((user) => user.username === 'Samantha'),
+  ];
 
-  const newArr = [...users, Samantha];
-  console.log(newArr);
+  console.log(newUsers);
 }
 
 main();
+
+async function main2() {
+  const users = await getUsers();
+  const i = users.findIndex((user) => user.username === 'Samantha');
+  const newUsers = [
+    ...users.slice(0, i),
+    ...users.slice(i + 1),
+    ...users.slice(i, i + 1),
+  ];
+  console.log(newUsers);
+}
+main2();
